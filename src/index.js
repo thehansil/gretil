181b286af8391f40d8d6270c8977851bf6ec96fa
Jacob Hansil <jacobhansil@gmail.com>
2024-04-19 16:38:42 -0400

    updated intents and added package start

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 import * as fs from 'node:fs';
 import * as path from 'node:path';
-import { Client, Collection, Events, GatewayIntentBits  } from 'discord.js';
+import { Client, Collection, Events, GatewayIntentBits , IntentsBitField  } from 'discord.js';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
@@ -10,37 +10,46 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const client = new Client({
 	intents: [
-		GatewayIntentBits.Guilds,
-		GatewayIntentBits.GuildMessages,
-		GatewayIntentBits.MessageContent,
-		GatewayIntentBits.GuildMembers,
+		IntentsBitField.Flags.Guilds,
+		IntentsBitField.Flags.GuildMembers,
+		IntentsBitField.Flags.GuildMessages,
+		IntentsBitField.Flags.MessageContent,
 	],
 });
-// import * as commands from 'commands/*';
+/**
+import * as commands from 'commands/*';
 
 
-// client.commands = new Collection();
+client.commands = new Collection();
 
-// const commandsPath = path.join(__dirname, 'commands');
-// const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
-// for (const file of commandFiles) {
-// 	const filePath = path.join(commandsPath, file);
-// 	const command = require(filePath);
-// 	// Set a new item in the Collection with the key as the command name and the value as the exported module
-// 	if ('data' in command && 'execute' in command) {
-// 		client.commands.set(command.data.name, command);
-// 	} else {
-// 		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-// 	}
-// }
-
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`)
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
+ */
+client.on('ready', (c) => {
+  console.log(`Logged in as ${c.user.tag}!`)
 })
 
 client.once(Events.ClientReady, c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
+	console.log(`✅ Ready! Logged in as ${c.user.tag}`);
 });
+
+client.on('messageCreate', (message) => {
+	if (message.author.bot) { return; }
+
+	if (message.content?.toLowerCase() === 'hello') {
+		message.reply('Hi!');
+	}
+})
 
 client.login(process.env.TOKEN)
