c8294784d09f519cdb024f553215c078de859c54
Jacob Hansil <jacobhansil@gmail.com>
2024-04-29 18:01:25 -0400

    wip

--- src/index.js
+++ src/index.js
@@ -3,57 +3,54 @@ dotenv.config();
 import { Client, Events, GatewayIntentBits, Guild, ChannelType } from "discord.js";
 
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMembers,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-  ],
+   intents: [
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildMembers,
+      GatewayIntentBits.GuildMessages,
+      GatewayIntentBits.MessageContent,
+   ],
 });
 
 client.once(Events.ClientReady, (c) => {
-  console.log(`✅ Ready! Logged in as ${c.user.tag}`);
+   console.log(`✅ Ready! Logged in as ${c.user.tag}`);
 });
 
 client.on("messageCreate", (message) => {
-  if (message.author.bot) return;
+   if (message.author.bot) return;
 
-  if (message.content?.toLowerCase() === "hello") {
-    message.reply("Hi!");
-  }
+   if (message.content?.toLowerCase() === "hello") {
+      message.reply("Hi!");
+   }
 });
 
 client.on("interactionCreate", (interaction) => {
-  if (!interaction.isChatInputCommand()) return;
+   if (!interaction.isChatInputCommand()) return;
 
-  if (interaction.commandName === "hey") {
-    interaction.reply("Hi!");
-  }
+   if (interaction.commandName === "update-channel-names") {
+      let newNames = interaction.options.get("names").value;
+      if (newNames !== null && newNames.includes(",")) {
+         newNames = newNames.split(",");
+         let voiceChannels = client.guilds.cache
+            .get(process.env.GUILD_ID)
+            .channels.cache.filter((c) => c.type === ChannelType.GuildVoice);
+         //.map((c) => c.id);
+         // console.log(voiceChannels);
+         voiceChannels.forEach((channel, i) => {
+            console.log(`Old Name: ${channel.name} - New Name: ${newNames[i]}`);
+            channel.setName(newNames[0]);
+         });
 
-  if (interaction.commandName === "update-channel-names") {
-    let newNames = interaction.options.get("names").value;
-    if (newNames !== null && newNames.includes(",")) {
-      newNames = newNames.split(",");
-      let allChannels = client.guilds.cache.get(process.env.GUILD_ID).channels.cache;
-      let voiceChannels = allChannels.filter((c) => c.type === ChannelType.GuildVoice);
-      voiceChannels.forEach((channel) => {
-        let i = 0;
-        console.log(`Old Name: ${channel.name} - New Name: ${newNames[i]}`);
-        channel.setName(newNames[i]);
-        console.log(`New Name: ${channel.name}`);
-        i++;
-      });
-      interaction.reply({
-        content: "Channel names updated!",
-        ephemeral: true,
-      });
-    } else {
-      interaction.reply({
-        content: "The channel names given need to have a ',' between each name",
-        ephemeral: true,
-      });
-    }
-  }
+         interaction.reply({
+            content: "Channel names updated!",
+            ephemeral: true,
+         });
+      } else {
+         interaction.reply({
+            content: "The channel names given need to have a ',' between each name",
+            ephemeral: true,
+         });
+      }
+   }
 });
 
 client.login(process.env.TOKEN);
--- src/register-commands.js
+++ src/register-commands.js
@@ -3,36 +3,32 @@ dotenv.config();
 import { REST, Routes, ApplicationCommandOptionType } from "discord.js";
 
 const commands = [
-  {
-    name: "update-channel-names",
-    description: "Updates the channel names of the server",
-    options: [
-      {
-        name: "names",
-        description: "Comma separated channel names.",
-        type: ApplicationCommandOptionType.String,
-        required: true,
-      },
-    ],
-  },
+   {
+      name: "update-channel-names",
+      description: "Updates the channel names of the server",
+      options: [
+         {
+            name: "names",
+            description: "Comma separated channel names.",
+            type: ApplicationCommandOptionType.String,
+            required: true,
+         },
+      ],
+   },
 ];
 
 const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
 
 (async () => {
-  try {
-    console.log("Registering slash commands...");
+   try {
+      console.log("Registering slash commands...");
 
-    await rest.put(
-      Routes.applicationGuildCommands(
-        process.env.CLIENT_ID,
-        process.env.GUILD_ID
-      ),
-      { body: commands }
-    );
+      await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), {
+         body: commands,
+      });
 
-    console.log("Slash commands were registered successfully.");
-  } catch (error) {
-    console.error(`There was an error: ${error}`);
-  }
+      console.log("Slash commands were registered successfully.");
+   } catch (error) {
+      console.error(`There was an error: ${error}`);
+   }
 })();
