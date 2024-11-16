64ee887a6ac234046488cc3b1e5d2cc64fc32e8f
Jacob Hansil <jacobhansil@gmail.com>
2024-11-15 21:15:06 -0500

    replaced dotenv with dotenvx

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import * as dotenv from "dotenv";
+import * as dotenv from "@dotenvx/dotenvx";
 dotenv.config();
 import { Client, Events, GatewayIntentBits, Guild, ChannelType } from "discord.js";
 
