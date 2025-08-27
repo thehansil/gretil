4be72d7bdb9e89fd758f048fe07e97b023cb6f14
Jacob Hansil <jacobhansil@gmail.com>
2025-08-27 16:47:48 -0400

    added message to @ friend who hates onions

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -30,6 +30,28 @@ const event = {
         console.error("Failed to provide link", error);
       }
     }
+
+    if (message.content?.toLowerCase().includes("onion")) {
+      const user = await message.guild.members
+        .fetch(process.env.ONION_HATER_ID)
+        .catch(() => null);
+      if (!user) return;
+      await message.react("🧅");
+      const randomNumber = Math.floor(Math.random() * 3) + 1;
+      let response: string;
+      switch (randomNumber) {
+        case 1:
+          response = `🚨🚨 Onion alert level RED! Paging ${user}`;
+          break;
+        case 2:
+          response = `🧅Onions🧅 have entered the chat. ${user}`;
+          break;
+        case 3:
+          response = `👀 Did someone just say… onions? ${user} is NOT going to like this.`;
+          break;
+      }
+      await message.channel.send(response);
+    }
   },
 };
 
