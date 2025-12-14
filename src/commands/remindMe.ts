0555b17662c78bf03edfccdd4ac5441336db70fd
Jacob Hansil <jacobhansil@gmail.com>
2025-12-13 21:05:16 -0500

    removed commented code

--- src/commands/remindMe.ts
+++ src/commands/remindMe.ts
@@ -7,8 +7,6 @@ import {
   TextInputBuilder,
   TextInputStyle,
 } from "discord.js";
-import mongoose from "mongoose";
-import Reminder from "../models/Reminder.js";
 
 const command = {
   data: new ContextMenuCommandBuilder()
@@ -34,28 +32,6 @@ const command = {
     modal.addComponents(row);
 
     await interaction.showModal(modal);
-
-    // try {
-    //   await mongoose.connect(process.env.MONGODB_URI);
-    //   await Reminder.create({
-    //     userId: interaction.user.id,
-    //     messageId: interaction.targetId,
-    //     channelId: interaction.channelId,
-    //     //TODO: Get time input from modal
-    //     remindAt: new Date(Date.now() + 3 * 60 * 1000),
-    //   });
-    //   await interaction.reply({
-    //     content: `✅ Got it! I’ll DM you the reminder.`,
-    //     ephemeral: true,
-    //   });
-    // } catch (error) {
-    //   await interaction.reply({
-    //     content: `There was an error setting up your reminder. Please try again later. \n ${error}`,
-    //     ephemeral: true,
-    //   });
-    // } finally {
-    //   mongoose.connection.close();
-    // }
   },
 };
 
